3b79e3d2a5aba6e8ea3d3dbb47069cd23b74b539
keating <ashmitbhardwaj05@gmail.com>
2023-06-21 00:38:17 +0530

    api

--- 23buildapi/main.go
+++ 23buildapi/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"strconv"
@@ -50,7 +51,7 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(params)
 
 	// loop through fake db to find the course matching the course id
-	for _,course := range courses {
+	for _, course := range courses {
 		if course.CourseId == params["id"] {
 			json.NewEncoder(w).Encode(course)
 			return
@@ -60,7 +61,7 @@ func getOneCourse(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func createCourse(w http.ResponseWriter, r *http.Request)  {
+func createCourse(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
 	// what if body is empty
@@ -77,6 +78,13 @@ func createCourse(w http.ResponseWriter, r *http.Request)  {
 		return
 	}
 
+	for _, c := range courses {
+		if c.CourseName == course.CourseName {
+			json.NewEncoder(w).Encode("Duplicate!!")
+			return
+		}
+	}
+
 	// generate unique id string
 	// append course in courses
 	rand.Seed(time.Now().UnixNano())
@@ -86,14 +94,14 @@ func createCourse(w http.ResponseWriter, r *http.Request)  {
 	return
 }
 
-func updateCourse(w http.ResponseWriter,r *http.Request)  {
-	w.Header().Set("Content-Type","application/json")
+func updateCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
 	// loop, id , remove, add with new id
-	for index,course := range courses {
+	for index, course := range courses {
 		if course.CourseId == params["id"] {
-			courses = append(courses[:index],courses[index+1:]...)
+			courses = append(courses[:index], courses[index+1:]...)
 			var newCourse Course
 			_ = json.NewDecoder(r.Body).Decode(&newCourse)
 			newCourse.CourseId = params["id"]
@@ -105,13 +113,13 @@ func updateCourse(w http.ResponseWriter,r *http.Request)  {
 	json.NewEncoder(w).Encode("No course found with the given id")
 }
 
-func deleteCourse(w http.ResponseWriter,r *http.Request)  {
-	w.Header().Set("Content-Type","application/json")
+func deleteCourse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
 
-	for index,course := range courses {
+	for index, course := range courses {
 		if course.CourseId == params["id"] {
-			courses = append(courses[:index],courses[index+1:]...)
+			courses = append(courses[:index], courses[index+1:]...)
 			json.NewEncoder(w).Encode("Deleted the course successfully")
 			return
 		}
@@ -120,5 +128,29 @@ func deleteCourse(w http.ResponseWriter,r *http.Request)  {
 }
 
 func main() {
+
+	fmt.Println("API Tutorial")
+	r := mux.NewRouter()
+
+	// seeding with fake data
+	courses = append(courses, Course{CourseId: "1", CourseName: "Life",
+		CoursePrice: "Infinite", Author: &Author{FullName: "Charles Bukowski", Website: "Don't have one"},
+	})
+	courses = append(courses, Course{CourseId: "2", CourseName: "Dictatorship",
+		CoursePrice: "Zero", Author: &Author{FullName: "George Orwell", Website: "They blocked it!"},
+	})
+	courses = append(courses, Course{CourseId: "3", CourseName: "Piracy",
+		CoursePrice: "A million bucks and a few shanties", Author: &Author{FullName: "Captain Jack Sparrow", Website: "On the way darling;)"},
+	})
+
+	// routing
+	r.HandleFunc("/", serveHome).Methods("GET")
+	r.HandleFunc("/courses", getAllCourses).Methods("GET")
+	r.HandleFunc("/courses/{id}", getOneCourse).Methods("GET")
+	r.HandleFunc("/course", createCourse).Methods("POST")
+	r.HandleFunc("/course/{id}", updateCourse).Methods("PUT")
+	r.HandleFunc("/course/{id}", deleteCourse).Methods("DELETE")
+	// listen and serve on a port
+	log.Fatal(http.ListenAndServe(":4000", r))
 
 }
